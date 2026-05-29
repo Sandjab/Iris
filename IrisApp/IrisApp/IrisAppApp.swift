7b76724394da5db7bf5d196bf2ce2535184d7aa8
Jean-Paul Gavini <46782050+Sandjab@users.noreply.github.com>
2026-05-29 21:47:23 +0200

    chore(phase-6.1b): bootstrap IrisApp.xcodeproj via Xcode (LSUIElement to follow)

---
+++ IrisApp/IrisApp/IrisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisAppApp.swift
+//  IrisApp
+//
+//  Created by Jean-Paul Gavini on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct IrisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
